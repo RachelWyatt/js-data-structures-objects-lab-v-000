be31fdb2af6ac1511fa17715194e4f6ffc6adb16
Rachel Wyatt <rw126@evansville.edu>
2019-04-09 17:18:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value) {
- return driver.assign({}, obj, { [key]: value });
+ return driver.assign({}, driver, { [key]: value });
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
